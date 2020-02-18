ce91cbf93659e75089ca344f5db1fa323c53ca41
Hao Zhi Yen <michaelyen@MYen.local>
2020-02-18 18:18:44 +0800

    added addItem and removeItem functionality to checkout item component (finished section 8)

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -5,7 +5,7 @@ import CustomButton from '../custom-button/custom-button.component';
 import {auth, signInWithGoogle} from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
-import { subnetMatch } from 'ipaddr.js';
+//import { subnetMatch } from 'ipaddr.js';
 
 class SignIn extends React.Component {
     constructor(props) {
